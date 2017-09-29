12987338ddcf0e5a9fefcb54a3aafed243256174
yangbinqi <ybq@outlook.com>
2017-09-29 10:58:33 +0800

    add 404 page

---
+++ SunShine.Web/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SunShine.EF;
+using SunShine.Model;
+using SunShine.BLL;
+
+namespace SunShine.Web.Controllers
+{
+    public class ErrorController: Controller
+    {
+        public ActionResult Error404() {
+             return View();
+
+        }
+    }
+}
--- SunShine.Web/Global.asax.cs
+++ SunShine.Web/Global.asax.cs
@@ -17,5 +17,14 @@ namespace SunShine.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+            if (ex is HttpException && ((HttpException)ex).GetHttpCode() == 404)
+            {
+                Response.Redirect("~/Error/Error404");
+            }
+        }
     }
 }
